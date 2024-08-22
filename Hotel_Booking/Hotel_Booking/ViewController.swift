1547abe951e5675abf874f3e3697a4385a0ddf3a
Yan <140942445+yano49@users.noreply.github.com>
2024-08-22 21:31:53 +0700

    Hotel_Booking_Template

---
+++ Hotel_Booking/Hotel_Booking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotel_Booking
+//
+//  Created by Knight on 22/8/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
