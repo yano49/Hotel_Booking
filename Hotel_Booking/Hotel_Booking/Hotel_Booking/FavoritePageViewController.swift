aaa27435751819ff87d1e0ea913079d78ad12633
Vanessa <vvvanessa246@gmail.com>
2024-09-28 03:12:28 +0700

    Add files via upload
    HomePage, SearchPageUI

---
+++ Hotel_Booking/Hotel_Booking/Hotel_Booking/FavoritePageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritePageViewController.swift
+//  Hotel_Booking
+//
+//  Created by SHIN MIN  on 26/09/2024.
+//
+
+import UIKit
+
+class FavoritePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
