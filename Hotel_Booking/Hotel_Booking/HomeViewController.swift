d0e3a5a63fe94e79ea8af13830b2aba294401239
Yan <140942445+yano49@users.noreply.github.com>
2024-09-27 16:30:57 +0700

    Add files via upload

---
+++ Hotel_Booking/Hotel_Booking/HomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewController.swift
+//  Hotel_Booking
+//
+//  Created by Knight on 27/9/2567 BE.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var greetingUser: UILabel!
+    @IBOutlet weak var RecommendLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func userProfile(_ sender: Any) {
+    }
+    
+
+
+}
